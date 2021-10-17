c6b229461574a5a87e9d707495ba6eb338f6c3f4
DenisPALD <masgutovmihail@gmail.com>
2021-10-17 21:01:30 +0500

    GUI ver3.0; title bar, tray is fully working now

--- ui/main_window.py
+++ ui/main_window.py
@@ -1,7 +1,9 @@
+import sys
+
 from PyQt5 import uic
-from PyQt5.QtCore import QPropertyAnimation, QEasingCurve, Qt
+from PyQt5.QtCore import QPropertyAnimation, QEasingCurve, Qt, QPoint
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QSystemTrayIcon, QAction, QStyle, qApp, QMenu
 
 
 class MainWindow(QMainWindow):
@@ -21,10 +23,27 @@ class MainWindow(QMainWindow):
         self.settings_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.settings_page))
         self.edit_button.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.edit_page))
 
+        self.create_tray()
+
+        self.close_button.clicked.connect(lambda: exit())
+        self.tray_button.clicked.connect(lambda: self.hide())
+
+        self.fullscreen = False
+        self.fullscreen_button.clicked.connect(self.fullscreen_function)
+
+
+
+        self.dragPos = QPoint()
+
+        self.LABEL_HEIGHT = self.app_name_label.height()
+        self.StartWidth = 60
+        self.EndWidth = 250
+
+
     def open_menu(self):
         if not self.toggled:
-            self.animation.setStartValue(60)
-            self.animation.setEndValue(250)
+            self.animation.setStartValue(self.StartWidth)
+            self.animation.setEndValue(self.EndWidth)
 
             self.home_button.setIcon(QIcon())
             self.home_button.setText("Главная")
@@ -36,8 +55,8 @@ class MainWindow(QMainWindow):
             self.edit_button.setText("Все карточки")
 
         else:
-            self.animation.setStartValue(250)
-            self.animation.setEndValue(60)
+            self.animation.setStartValue(self.EndWidth)
+            self.animation.setEndValue(self.StartWidth)
 
             self.home_button.setIcon(QIcon("ui/home.png"))
             self.home_button.setText("")
@@ -50,3 +69,44 @@ class MainWindow(QMainWindow):
 
         self.toggled = not self.toggled
         self.animation.start()
+
+    def fullscreen_function(self):
+        if not self.fullscreen:
+            self.showMaximized()
+            self.fullscreen_button.setIcon(QIcon("ui/fullscreen_exit.png"))
+        else:
+            self.showNormal()
+            self.fullscreen_button.setIcon(QIcon("ui/fullscreen.png"))
+
+        self.fullscreen = not self.fullscreen
+
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() == Qt.LeftButton and event.pos().y() <= self.LABEL_HEIGHT:
+            self.move(self.pos() + event.globalPos() - self.dragPos)
+            self.dragPos = event.globalPos()
+            event.accept()
+
+    def mouseDoubleClickEvent(self, event):
+        if event.pos().y() <= self.LABEL_HEIGHT:
+            self.fullscreen_function()
+
+
+    def create_tray(self):
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(QIcon("ui/repeat.svg"))
+
+        show_action = QAction("Show", self)
+        quit_action = QAction("Exit", self)
+        hide_action = QAction("Hide", self)
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        quit_action.triggered.connect(lambda: exit())
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        tray_menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.show()
